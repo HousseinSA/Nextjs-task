58e6b8fc58e4a8d7d56cafbd21ea6689c31a0652
hussein <husseinsedoum@gmail.com>
2024-12-24 10:31:47 +0000

    update

--- app/hooks/usePageLabel.tsx
+++ app/hooks/usePageLabel.tsx
@@ -3,26 +3,32 @@ import { useSelector } from "react-redux"
 import { usePathname } from "next/navigation"
 import { RootState } from "@lib/types/HeaderTypes"
 import { languages } from "@lib/types/languages/languages"
+import { useEffect, useState } from "react"
 
 export const usePageLabel = () => {
   const pathname = usePathname()
   const language = useSelector((state: RootState) => state.header.language)
+  const [label, setLabel] = useState<string | null>(null)
+  const [titlePosition, setTitlePosition] = useState<string>("text-left")
 
-  const getLabelByPathname = (pathname: string, language: string) => {
-    const currentLanguage = languages.find((lang) => lang.code !== language)
-    if (!currentLanguage) return null
+  useEffect(() => {
+    const getLabelByPathname = (pathname: string, language: string) => {
+      const currentLanguage = languages.find((lang) => lang.code !== language)
+      if (!currentLanguage) return null
 
-    const navItems = currentLanguage.content.navItems
-    for (const key in navItems) {
-      if (navItems[key].route === pathname) {
-        return navItems[key].label
+      const navItems = currentLanguage.content.navItems
+      for (const key in navItems) {
+        if (navItems[key].route === pathname) {
+          return navItems[key].label
+        }
       }
+      return null
     }
-    return null
-  }
 
-  const label = getLabelByPathname(pathname, language)
-  const titlePosition = language === "ar" ? "text-right" : "text-left"
+    const label = getLabelByPathname(pathname, language)
+    setLabel(label)
+    setTitlePosition(language === "ar" ? "text-right" : "text-left")
+  }, [pathname, language])
 
   return { label, titlePosition }
 }
